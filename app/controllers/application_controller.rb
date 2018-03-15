02cffaf0516457a61edc01124457c7113886a44d
Web Master <24606127+webmaster7788@users.noreply.github.com>
2018-03-15 17:45:06 -0500

    fix heroku issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery prepend: true
   # protect_from_forgery with: :exception
-  protect_from_forgery
+  protect_from_forgery with: :null_session
 
   before_action :authorize_current_activity, :unless => :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
